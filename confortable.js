7e66b7a3a9b80bf9dcc29302d3d7edefc8c5444d
clux <analsandblaster@gmail.com>
2012-07-13 15:52:24 +0100

    stop infinite loops on windows when start dir is on different drive than HOME

--- confortable.js
+++ confortable.js
@@ -2,18 +2,21 @@ var fs   = require('fs')
   , path = require('path')
   , exists = fs.existsSync || path.existsSync;
 
-module.exports = function (name, cwd) {
-  var cfg;
+module.exports = function (name, start) {
+  start = start || process.cwd();
+  var relative = path.relative(process.env.HOME, start)
+    , differentDrive = (relative === start)
+    , isAbove = (relative.slice(0, 2) === '..')
+    , cfg;
 
-  cwd = cwd || process.cwd();
-  if (path.relative(process.env.HOME, cwd).slice(0, 2) === '..') {
-    // cwd is outside home, check cwd only
-    cfg = path.join(cwd, name);
+  if (differentDrive || isAbove) {
+    // start is outside home, check start only
+    cfg = path.join(start, name);
     return exists(cfg) ? cfg : null;
   }
   else {
-    // cwd is somewhere under HOME start in cwd and go up until we hit HOME
-    var dir = cwd;
+    // start is somewhere under HOME start, so start there and go up until we hit HOME
+    var dir = start;
     while (path.relative(process.env.HOME, dir).slice(0, 2) !== '..') {
       cfg = path.join(dir, name);
       if (exists(cfg)) {
